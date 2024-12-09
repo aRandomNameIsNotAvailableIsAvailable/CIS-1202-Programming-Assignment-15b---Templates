6606d1c859227048bc7371c014514dde1531acd2
Kyle Baker <kyle.baker@sinclair.edu>
2024-12-08 20:19:00 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+/*
+Name: Kyle Baker
+Date: 12/08/2024
+Project Name: CIS 1202 Programming Assignment #15b - Templates
+Project Description: Write templated functions to perform math on varying numeric data types.
+*/
+
+// libraries
+#include<cmath>
+#include<iostream>
+
+// namespace
+using namespace std;
+	//											+++Start Main+++
+
+int main() {
+
+	int a = 7;
+	float b = 5.0;
+	double c = 3;
+
+	cout << a;
+	cout << endl;
+	cout << b;
+	cout << endl;
+	cout << c;
+	cout << endl;
+
+	//											+++END MAIN+++
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+
+}
